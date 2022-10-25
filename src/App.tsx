5b138d7d83b3501b200086fed07de8402a5cc24b
Philip Lindberg <philip.c.lindberg@gmail.com>
2022-10-25 11:42:33 +0100

    Add photoswipe

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import PhotoSwipeLightbox from 'photoswipe/lightbox'
+
 function App() {
   return (
     <div className="App">
